043302e4a28d1a5796ffaea427507ce10439a6f0
Yoshisora3 <sora.20020315@icloud.com>
2026-01-05 13:44:07 +0900

    関数を追加、削除するメモをIDで識別

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, request, render_template
 from sqlalchemy import create_engine, Column, Integer, String
 from sqlalchemy.orm import declarative_base, sessionmaker
+from utils import id_search
 
 # Flaskアプリの準備
 app = Flask(__name__)
@@ -25,7 +26,7 @@ def index():
     users = session.query(User).all()
     return render_template('index.html', users=users, notecount=len(users))
 
-# 名前を追加する
+# メモの追加
 @app.route('/add', methods=['GET', 'POST'])
 def add_user():
     if request.method == 'POST' and request.form['addnote'] != "":
@@ -43,72 +44,41 @@ def add_user():
     users = session.query(User).all()
     return render_template('index.html', users=users, notecount=len(users))
 
+# メモの削除
 @app.route('/del', methods=['GET', 'POST'])
 def del_user():
     if request.method == 'GET':
         return "エラー GET送信されました"
-    
+        
     if len(session.query(User).all()) == 0:
         return render_template('index.html', users=session.query(User).all(), notecount=len(session.query(User).all()))
-    if request.method == 'POST' and request.form['delnote'] != "":
-        delname = request.form['delnote']
-        delid = delname[0]
-
-        #メモが10個以上あるとうまく消せないので処理を追加
-        flag = 0
-        for i in  delname:
-            if i == ":":
-                break
-            if flag == 1:
-                delid += i
-            else:
-                flag = 1
-        
-        user = session.query(User).filter_by(id=delid).first()
-        session.delete(user)
-        session.commit()
-
-        users = session.query(User).all()
-        return render_template('index.html', users=users, notecount=len(users))
     
-    
-    return "エラー4"
+    session.delete(id_search(session, request.form['delid'], User, request.method))
+    session.commit()
+    users = session.query(User).all()
 
+    return render_template('index.html', users=users, notecount=len(users))
+
+# メモの更新
 @app.route('/update', methods=['GET', 'POST'])
 def update_user():
     if request.method == 'GET':
         return "エラー GET送信されました"
+    
     users = session.query(User).all()
     if len(users) == 0:
         return render_template('index.html', users=users, notecount=len(users))
-    if request.method == 'POST' and request.form['upnote'] != "":
-        upname = request.form['upnote']
-        upid = upname[0]
 
-        #メモが10個以上あるとうまく消せないので処理を追加
-        flag = 0
-        for i in  upname:
-            if i == ":":
-                break
-            if flag == 1:
-                upid += i
-            else:
-                flag = 1
-            
-        user = session.query(User).filter_by(id=upid).first()
+    return render_template('update.html', users=id_search(session, request.form['upnote'], User, request.method))
 
-    return render_template('update.html', users=user)
-
+# メモ更新の完了
 @app.route('/update_comp', methods=['GET', 'POST'])
 def updatecomp_user():
     if request.method == 'GET':
         return "エラー GET送信されました"
     
-    upname = request.form['upnote']
-    
-    upid = request.form['upid']
-    user = session.query(User).filter_by(id=upid).first()
-    user.name = upname
+    user = id_search(session, request.form['upid'], User, request.method)
+    user.name = request.form['upnote']
     session.commit()
 
     users = session.query(User).all()
--- utils.py
+++ utils.py
@@ -1,2 +1,6 @@
-def hello():
-    print("hello")
+def id_search(session, search_id, User, method):
+    if method == 'POST':
+        
+        user = session.query(User).filter_by(id=search_id).first()
+
+        return user
